91d526353c7b9f5f8e8f3ffa50efeb0f6b77cd9a
Tanishq Choudhary <125427226+tanishq2445@users.noreply.github.com>
2025-08-19 12:35:36 +0530

    Added register route (#2)

--- Backend/Routes/userRoute.js
+++ Backend/Routes/userRoute.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const loginUser = require("../controller/userController")
+const {loginUser,registerUser} = require("../controller/userController")
 
 const router = express.Router();
 
-// router.post("/register");
-router.post("/login",loginUser);
+router.post("/register",registerUser);
+// router.post("/login",loginUser);
 // router.get("/getProfile");
 
 
