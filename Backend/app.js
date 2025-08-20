ebd83be9c402323ccfc67314ed6f9b94f438494b
Taran121 <taranpreet2454.be22@chitkara.edu.in>
2025-08-20 21:41:43 +0530

    fixed bugs 2.0

--- Backend/app.js
+++ Backend/app.js
@@ -2,12 +2,16 @@ const express = require("express");
 const connectionDb = require("./config/db");
 const userRoutes = require("./Routes/userRoute");
 const sessionRoute = require("./Routes/SessionRoute");
+const cookieParser = require("cookie-parser");
+
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
+
 app.use("/api/user",userRoutes);
-app.use("api/session",sessionRoute)
+app.use("/api/session",sessionRoute);
 
 app.get("/",function(req,res){
     res.send("Hello world!");
